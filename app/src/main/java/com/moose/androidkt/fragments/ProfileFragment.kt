dc30876567f5f4ef77f682a884bd4c8e9fbc54e2
linus muema <linus.m.muema@gmail.com>
2020-12-08 11:56:45 +0300

    - changed the UI ProfileFragment.kt

--- app/src/main/java/com/moose/androidkt/fragments/ProfileFragment.kt
+++ app/src/main/java/com/moose/androidkt/fragments/ProfileFragment.kt
@@ -6,20 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.moose.androidkt.R
 import com.moose.androidkt.data.Data
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class ProfileFragment : Fragment() {
-
-    private val args: ProfileFragmentArgs by navArgs()
-    private val user = Data.getUser(args.userId)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
 }
