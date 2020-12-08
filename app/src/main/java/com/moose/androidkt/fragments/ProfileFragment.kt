0b1d2c35eec507e71873bea78dd8c3f831f7feef
linus muema <linus.m.muema@gmail.com>
2020-12-08 11:52:37 +0300

    - added the profile page

--- app/src/main/java/com/moose/androidkt/fragments/ProfileFragment.kt
+++ app/src/main/java/com/moose/androidkt/fragments/ProfileFragment.kt
@@ -7,10 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.moose.androidkt.R
+import com.moose.androidkt.data.Data
 
 class ProfileFragment : Fragment() {
 
+    private val args: ProfileFragmentArgs by navArgs()
+    private val user = Data.getUser(args.userId)
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
