75f4822cce5c169f9f1c57807ec19e9dacc730d7
linus muema <linus.m.muema@gmail.com>
2020-12-08 12:42:49 +0300

    - finish the basic setup

--- app/src/main/java/com/moose/androidkt/fragments/UsersFragment.kt
+++ app/src/main/java/com/moose/androidkt/fragments/UsersFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.moose.androidkt.R
 import com.moose.androidkt.adapters.ListAdapter
 import com.moose.androidkt.data.Data
@@ -19,7 +21,9 @@ class UsersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val listAdapter = ListAdapter(Data.getUsers()){}
+        val listAdapter = ListAdapter(Data.getUsers()){
+            // Add the navigation here
+        }
 
         recyclerview.apply {
             setHasFixedSize(true)
