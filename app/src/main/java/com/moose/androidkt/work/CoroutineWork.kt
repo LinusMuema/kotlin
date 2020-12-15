8a5862c72931edf7e41dd3cb49b31c2231ac69b1
linus muema <linus.m.muema@gmail.com>
2020-12-15 15:30:02 +0300

    - change the coroutineScope

--- app/src/main/java/com/moose/androidkt/work/CoroutineWork.kt
+++ app/src/main/java/com/moose/androidkt/work/CoroutineWork.kt
@@ -5,18 +5,22 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.moose.androidkt.data.Data
 import com.moose.androidkt.db.AppDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class CoroutineWork(context: Context, params: WorkerParameters):CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
         val dao = AppDatabase.getDatabase(applicationContext).dao()
-        return try {
-            val user = Data.getCoroutineUser(inputData.getInt("USER_ID", 0))
-            dao.addCoroutineUser(user)
-            Result.success()
-        }catch (e: Exception){
-            Result.failure()
+        return withContext(Dispatchers.IO){
+            try {
+                val user = Data.getCoroutineUser(inputData.getInt("USER_ID", 0))
+                dao.addCoroutineUser(user)
+                Result.success()
+            }catch (e: Exception){
+                Result.failure()
+            }
         }
     }
 }
