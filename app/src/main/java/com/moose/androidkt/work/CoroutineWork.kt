c43f9fc445c5be413715a43ec8a0035bea58622c
linus muema <linus.m.muema@gmail.com>
2020-12-15 15:30:28 +0300

    - roolback

--- app/src/main/java/com/moose/androidkt/work/CoroutineWork.kt
+++ app/src/main/java/com/moose/androidkt/work/CoroutineWork.kt
@@ -5,22 +5,18 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.moose.androidkt.data.Data
 import com.moose.androidkt.db.AppDatabase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class CoroutineWork(context: Context, params: WorkerParameters):CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
         val dao = AppDatabase.getDatabase(applicationContext).dao()
-        return withContext(Dispatchers.IO){
-            try {
-                val user = Data.getCoroutineUser(inputData.getInt("USER_ID", 0))
-                dao.addCoroutineUser(user)
-                Result.success()
-            }catch (e: Exception){
-                Result.failure()
-            }
+        return try {
+            val user = Data.getCoroutineUser(inputData.getInt("USER_ID", 0))
+            dao.addCoroutineUser(user)
+            Result.success()
+        }catch (e: Exception){
+            Result.failure()
         }
     }
 }
